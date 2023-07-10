1c6121e164c6131345af7f1660bf889260ad6750
EdwardKuchumov <edwardkuchumov@gmail.com>
2023-07-10 11:07:27 +0500

    test

---
+++ js/array.js
@@ -0,0 +1,253 @@
+const langArr = {
+    // navigation
+    'logInLg': {
+        'ru': 'Войти',
+        'en': 'Log in'
+    },
+    'comLg': {
+        'ru': 'торговля',
+        'en': 'commerce'
+    },
+    'marketsLg': {
+        'ru': 'рынки',
+        'en': 'markets'
+    },
+    'сustomersLg': {
+        'ru': 'клиентам',
+        'en': 'сustomers'
+    },
+    'aboutLg': {
+        'ru': 'о нас',
+        'en': 'about us'
+    },
+    // first block
+    'titLg':{
+        'ru': 'криптовалютный рынок',
+        'en': 'cryptocurrency market'
+    },
+    'tradeLg':{
+        'ru': 'торгуйте криптовалютами на мировом финансовом рынке на конкурентоспособных условиях.',
+        'en': 'trade cryptocurrencies on the global financial market on competitive terms.'
+    },
+    'buttonLg':{
+        'ru': 'Открыть счет',
+        'en': 'Open an account'
+    },
+    // second block
+    'titbenefitLg':{
+        'ru': 'Преимущества торговли с «United Brokers»',
+        'en': 'Advantages of trading with United Brokers'
+    },
+    'ultraFastLg':{
+        'ru': 'Сверхбыстрое исполнение и высокая ликвидность',
+        'en': 'Ultra-fast execution and high liquidity'
+    },
+    'textUltraFastLg':{
+        'ru': '99,9% ордеров исполняются менее чем за 11.06 мс',
+        'en': '99.9% of orders are executed in less than 11.06 ms'
+    },
+    'typeCountLg':{
+        'ru': 'Типы счетов',
+        'en': 'Types of accounts'
+    },
+    'abilityTradeLg':{
+        'ru': 'Возможность торговатьMini, Standard, Classic и ProInvest счетами с одного устройства  ',
+        'en': 'Ability to trade Mini, Standard, Classic and ProInvest accounts From One Device'
+    },
+    'globalStandartLg':{
+        'ru': 'Мировые стандарты безопасности',
+        'en': 'Global safety standards'
+    },
+    'choseLg':{
+        'ru': 'Выбирайте надежного брокера, который работает с клиентами более чем в 83 странах мира',
+        'en': 'Choose a reliable broker that works with clients in more than 83 countries'
+    },
+    'toolsLg':{
+        'ru': '150+ инструментов',
+        'en': '150+ tools'
+    },
+    'forexLg':{
+        'ru': 'Forex, акции, фьючерсы, индексы, металлы и энергоресурсы',
+        'en': 'Forex, stocks, futures, indices, metals and energy'
+    },
+    'multiLangLg':{
+        'ru': 'Мультиязычная поддержка 24/5',
+        'en': 'Multilingual support 24/5'
+    },
+    'feelLg':{
+        'ru': 'Почувствуйте себя уверенно, зная,что наша служба поддержки доступна для вас в любое время дня и ночи, с понедельника по пятницу',
+        'en': 'Feel confident, knowing,that our support team is available for you at any time of the day or night, Monday through Friday'
+    },
+    // three block
+    'chooseLg':{
+        'ru': 'Выберите свою платформу',
+        'en': 'Choose your platform'
+    },
+    'readmoreLg':{
+        'ru': 'Подробнее',
+        'en': 'Read more'
+    },
+    'takeLg':{
+        'ru': 'Переходите на новый уровень трейдинга с профессионально оснащенной платформой «United Brokers». Доступны версии для ПК, веб и смартфонов.',
+        'en': 'Take your trading to the next level with the professionally equipped United Brokers platform. Available in PC, web and smartphone versions.'
+    },
+    // four block
+    'questLg':{
+        'ru': 'Вопросы и ответы',
+        'en': 'Questions and answers'
+    },
+    'whatLg':{
+        'ru': 'Что такое криптовалюты?',
+        'en': 'What are cryptocurrencies?'
+    },
+    'cryptLg':{
+        'ru': '<span>Криптовалюты</span> – относительно недавно возникшее явление в финансовом мире, стремительно набирающее популярность. Это цифровые деньги, в основе которых лежит децентрализованная открытая база – блокчейн, функционирующая на основе криптографических методов. Криптовалюты не имеют физического облика, они существуют лишь в электронном виде.',
+        'en': '<span>Cryptocurrencies</span> - are a relatively recent phenomenon in the financial world that is rapidly gaining popularity. It is digital money based on a decentralized open base - blockchain, which functions on the basis of cryptographic methods. Cryptocurrencies have no physical form, they exist only in electronic form.'
+    },
+    'crypt2Lg':{
+        'ru': '<span>Криптовалюты</span> – относительно недавно возникшее явление в финансовом мире, стремительно набирающее популярность. Это цифровые деньги, в основе которых лежит децентрализованная открытая база – блокчейн, функционирующая на основе криптографических методов. Криптовалюты не имеют физического облика, они существуют лишь в электронном виде.',
+        'en': '<span>Cryptocurrencies</span> - are a relatively recent phenomenon in the financial world that is rapidly gaining popularity. It is digital money based on a decentralized open base - blockchain, which functions on the basis of cryptographic methods. Cryptocurrencies have no physical form, they exist only in electronic form.'
+    },
+    'advantageLg':{
+        'ru': 'Основные преимущества криптовалют',
+        'en': 'The main advantages of cryptocurrencies'
+    },
+    'startLg':{
+        'ru': 'Начать торговать',
+        'en': 'Start trading'
+    },
+    'downloadLg':{
+        'ru': 'Скачать приложение <br> United Brokers:',
+        'en': 'Download App <br> United Brokers:'
+    },
+    'lkLg':{
+        'ru': 'Личный кабинет',
+        'en': 'Personal Cabinet'
+    },
+    // trade
+    'tradeFooter':{
+        'ru': 'Торговля',
+        'en': 'Trade'
+    },
+    'platformFooter':{
+        'ru': 'Платформы',
+        'en': 'Platforms'
+    },
+    'typeFooter':{
+        'ru': 'Типы счетов',
+        'en': 'Types of accounts'
+    },
+    'termsFooter':{
+        'ru': 'Условия торговли',
+        'en': 'Terms of trade'
+    },
+    // markets
+    'marketsFooter':{
+        'ru': 'Рынки',
+        'en': 'Markets'
+    },
+    'currencyFooter':{
+        'ru': 'Валютный рынок',
+        'en': 'Currency market'
+    },
+    'stockFooter':{
+        'ru': 'Фондовый рынок',
+        'en': 'Stock market'
+    },
+    'commodityFooter':{
+        'ru': 'Товарный рынок',
+        'en': 'Commodity market'
+    },
+    'indexFooter':{
+        'ru': 'Индексы',
+        'en': 'Indexes'
+    },
+    'cryptoFooter':{
+        'ru': 'Криптовалюты',
+        'en': 'Cryptocurrencies'
+    },
+    // customers
+    'customFooter':{
+        'ru': 'Клиентам',
+        'en': 'Customers'
+    },
+    'analyticsFooter':{
+        'ru': 'Аналитика',
+        'en': 'Analytics'
+    },
+    'newsFooter':{
+        'ru': 'Новости',
+        'en': 'News'
+    },
+    'trainingFooter':{
+        'ru': 'Обучение',
+        'en': 'Training'
+    },
+    'tipsFooter':{
+        'ru': 'Советы трейдерам',
+        'en': 'Tips for traders'
+    },
+    'loyaltyFooter':{
+        'ru': 'Программа лояльности',
+        'en': 'Loyalty Program'
+    },
+    // about
+    'aboutFooter':{
+        'ru': 'О нас',
+        'en': 'About Us'
+    },
+    'companyFooter':{
+        'ru': 'О компаниии',
+        'en': 'About company'
+    },
+    'contactFooter':{
+        'ru': 'Контакты',
+        'en': 'Contact'
+    },
+    'documentFooter':{
+        'ru': 'Документы',
+        'en': 'Documents'
+    },
+    'securityFooter':{
+        'ru': 'Сохранность средств',
+        'en': 'Security of funds'
+    },
+    'vacanciesFooter':{
+        'ru': 'Вакансии',
+        'en': 'Vacancies'
+    },
+    // document
+    'docFooter':{
+        'ru': 'Документы',
+        'en': 'Documents'
+    },
+    'customerFooter':{
+        'ru': 'Клиентское соглашение',
+        'en': 'Customer Agreement'
+    },
+    'privateFooter':{
+        'ru': 'Политика конфиденциальности',
+        'en': 'Privacy Policy'
+    },
+    'refoundFooter':{
+        'ru': 'Политика возврата платежей',
+        'en': 'Refund policy'
+    },
+    'certificateFooter':{
+        'ru': 'Свидетельство о регистрации',
+        'en': 'Certificate of registration'
+    },
+    'amlFooter':{
+        'ru': 'Политика AML',
+        'en': 'AML Policy'
+    },
+    // policy
+    'policyUKFooter':{
+        'ru': '© United Brokers 2021. Все права защищены. Любое использование материалов сайта без разрешения запрещено.',
+        'en': '© United Brokers 2021. All rights reserved. Any use of site materials without permission is prohibited.'
+    },
+    'noticeFooter':{
+        'ru': 'Уведомление о рисках: Инвестирование в группы высокого риска: (Forex) и контрактами на разницу (CFD) является спекулятивной операцией, включая высокий риск, и не подходит для каждого инвестора. Вы можете понести как частичные, так и полные потери вложенных средств, поэтому мы не советуем инвестировать капитал, которым вы не можете рисковать. Вы должны знать о повышенном риске, связанном с использованием кредитного плеча. Мы настоятельно рекомендуем вам ознакомиться с условиями и услугами нашего веб-сайта, прежде чем начать пользоваться нашим сервисом.',
+        'en': 'Notice of Risk: Investing in high risk groups: (Forex) and Contracts for Difference (CFDs) is a speculative operation, including high risk, and is not suitable for every investor. You may sustain a loss of some or all of your deposited capital, therefore, we advise against investing capital that you cannot afford to lose. You should be aware of the increased risk associated with the use of leverage. We strongly recommend you to read the terms and services of our website before you start using our service.'
+    },
+}
---
+++ js/script.js
@@ -0,0 +1,42 @@
+// accordion
+const questBlock = document.querySelectorAll('.questions__titleBlock');
+questBlock.forEach((item)=>{
+    item.addEventListener('click', ()=>{
+        item.nextElementSibling.classList.toggle('hidden')
+        if(item.nextElementSibling.classList.contains('hidden')){
+            item.querySelector('.plus').style.backgroundColor = '#A0A0A0'
+        } else{
+            item.querySelector('.plus').style.backgroundColor = '#f4f4f4'
+        }
+    })
+})
+const allLang = ['ru', 'en']
+const select = document.querySelector('select')
+select.addEventListener('change', changeURLLanguage)
+
+function changeURLLanguage(){
+    let lang = select.value;
+    location.href = window.location.pathname + '#'+ lang;
+    location.reload()
+    
+}
+
+function changeLanguage(){
+    let hash = window.location.hash
+    hash = hash.substr(1)
+    if(!allLang.includes(hash)){
+        location.href = window.location.pathname + '#ru';
+        location.reload()
+    }
+    select.value = hash
+    // document.querySelector('.tit').innerHTML = langArr['tit'][hash];
+    for (let key in langArr){
+        let elem = document.querySelector('.tit-' + key)
+        
+        if(elem){
+            elem.innerHTML = langArr[key][hash]
+        }
+        
+    }
+}
+changeLanguage()
